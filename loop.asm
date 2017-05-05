7c39fedfd5122fffb840471d78b9ec013166c747
maben <www.maben@foxmail.com>
2017-05-05 18:59:23 +0800

    update loop

--- loop.asm
+++ loop.asm
@@ -1,11 +1,31 @@
-assume cs:code
-code segment
-  mov ax, 2
-  mov cx, 11
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
+
+.data
+
+count dword 9
+base dword 2
+sum dword 0
+
+.code
+
+main proc
+
+	mov ecx, count
+	mov eax, base
 s:
-  add ax, ax
-  loop s
-  mov ax, 4c00h
-  int 21h
-code ends
-end
+	add eax, base
+	loop s
+
+	mov eax, 0
+	mov ecx, count
+s2:
+	inc eax
+	loop s2
+
+	INVOKE ExitProcess, 0
+
+main endp
+end main
