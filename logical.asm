b03170b8d27d74776f56dd53df600a1d860c1d4a
maben <www.maben@foxmail.com>
2017-05-06 22:44:29 +0800

    update logical

--- logical.asm
+++ logical.asm
@@ -16,6 +16,28 @@ main proc
 	mov ebx, 1
 	AND eax, ebx
 
+	; OR
+	mov eax, 1
+	mov ebx, 0
+	OR eax, ebx
+
+	mov eax, 0
+	mov ebx, 0
+	OR eax, ebx
+
+	; XOR
+	mov eax, 1
+	mov ebx, 1
+	XOR eax, ebx
+
+	mov eax, 0
+	mov ebx, 0
+	XOR eax, ebx
+
+	mov eax, 1
+	mov ebx, 0
+	XOR eax, ebx
+
 	INVOKE ExitProcess, 0
 main endp
 
