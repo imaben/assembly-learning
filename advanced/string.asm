548417e90f4f31d8fbf23773749c7a641234087f
maben <www.maben@foxmail.com>
2017-05-15 22:01:52 +0800

    add word copy

--- advanced/string.asm
+++ advanced/string.asm
@@ -8,14 +8,24 @@ string1 BYTE "This is a source string", 0
 string1Len = ($-string1) - 1
 string2 BYTE 32
 
+word1 WORD 20 DUP(0FFFFFFFh)
+word2 WORD 20 DUP(?)
+
 .code
 main proc
-	; string copy
+	; byte copy
 	cld
 	mov esi, OFFSET string1
 	mov edi, OFFSET string2
 	mov ecx, string1Len
 	rep movsb
+
+	; word copy
+	cld
+	mov esi, OFFSET word1
+	mov edi, OFFSET word2
+	mov ecx, LENGTHOF word1
+	rep movsd
 
 	invoke ExitProcess,0
 main endp
