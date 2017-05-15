13ff1579849f0354f19d4e9df72d945464e41303
maben <www.maben@foxmail.com>
2017-05-15 22:27:01 +0800

    bug fix

--- advanced/string.asm
+++ advanced/string.asm
@@ -8,11 +8,11 @@ string1 BYTE "This is a source string", 0
 string1Len = ($-string1) - 1
 string2 BYTE 32
 
-word1 WORD 20 DUP(0FFFFFFFh)
-word2 WORD 20 DUP(?)
+word1 WORD 20 DUP(123h)
+word2 WORD 20 DUP(0)
 
-dword1 DWORD 20 DUP(0FFFFFFFh)
-dword2 DWORD 20 DUP(?)
+dword1 DWORD 20 DUP(123h)
+dword2 DWORD 20 DUP(0)
 
 .code
 main proc
@@ -39,3 +39,4 @@ main proc
 
 	invoke ExitProcess,0
 main endp
+end main
